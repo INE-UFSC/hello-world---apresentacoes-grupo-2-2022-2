02ebd75dbcf8f68cdfbab201f009f712a72c4165
Tális <talisbreda@gmail.com>
2022-08-26 11:42:00 -0300

    Adicinado membro 4 e editado o app.py

---
+++ membro4.py
@@ -0,0 +1,4 @@
+def hello():    
+	return '''Olá, eu sou o Tális Breda e tenho 18 anos. Tive contato com tecnologia desde pequeno, mas só no ensino médio começei a ter contato com a programação, 
+quando fiz curso técnico em Informática. Desde então, tenho estudado e praticado bastante através de projetos pessoais e estudos por conta. 
+Gosto bastante de jogos online e esportes no geral, e tenho grande ambição na área da programação'''
