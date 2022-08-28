110d27ec27aec9a6f18110c904c65dc60fea4f99
Maykon <maykon.marcos.junior@grad.ufsc.br>
2022-08-28 20:03:21 -0300

    informações preenchidas

--- membro2.py
+++ membro2.py
@@ -1,2 +1,11 @@
+# -*- coding: utf -8 -*-
 def hello():
-	return "Olá, eu sou o membro2" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+    apresentacao = 'Olá, meu nome é Maykon Marcos Junior e eu tenho 18 anos./n'
+    apresentacao += 'Gosto de ler, jogar truco e assistir séries/n'
+    apresentacao += '(embora já não faça nenhuma dessas coisas por falta de tempo)/n'
+    apresentacao += 'Não tenho experiência prévia com programação, fora o semestre anterior/n'
+    apresentacao += 'Quanto a essa disciplina, espero que o nome seja honesto'
+    apresentacao += '(diferente de POO I, que poderia ser renomeado para Python)'
+    return apresentacao
+#escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação
+#e o que você espera desta disciplina
