5aecc77fb9ebe55c13569e066bd73dcc5545a1f2
INE-UFSC <leosteffanileoleo@gmail.com>
2022-08-29 19:20:42 -0300

    alterado texto do membro1

--- membro1.py
+++ membro1.py
@@ -1,2 +1,4 @@
 def hello():
-	return "Olá, eu sou o membro1" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return '''Olá, eu sou o membro1 / nome: Leonardo Costa Steffani / idade: 19 
+hobbies: RPG de mesa, video games, desenhar / experiência prévia de programação: nenhuma fora do curso ;-; 
+o que você espera desta disciplina: compreender melhor a parte de POO e em programação no geral'''
